7cc59c198dc259926a93dc1d04af18f4207ce6b9
Curtis <brumfieldcurtis1996@gmail.com>
2022-09-05 11:09:17 -0500

    Added Todo form and title

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,13 @@
-
+import TodoForm from './TodoForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+    <div className='form'>
+    <h1 className='title'>Todo List</h1>
+      <TodoForm />
+      </div>
     </div>
   );
 }
---
+++ todolist/src/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoForm = () => {
+  return (
+    <div>
+        <div>
+            <input />
+            <button>Add Todo</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default TodoForm
